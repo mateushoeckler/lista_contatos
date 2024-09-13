4cc021d4227e2172e23ac03916fd71e4396d0876
mateushoeckler <mateushoeckler@gmail.com>
2024-09-13 08:18:00 -0300

    cupertino update

--- lib/date_picker_page.dart
+++ lib/date_picker_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -10,39 +11,48 @@ class DatePickerPage extends StatefulWidget {
 
 class _DatePickerPageState extends State<DatePickerPage> {
 
-  TextEditingController _dataController = TextEditingController();
   DateTime _dataSelecionada = DateTime.now();
-
-  Future<void> _selecionarData(BuildContext context) async {
-    final DateTime? dataEscolhida = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-
-    if (dataEscolhida != null) {
-      setState(() {
-        _dataSelecionada = dataEscolhida;
-        _dataController.text = DateFormat('dd/MM/yyyy').format(_dataSelecionada);
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: TextField(
-            controller: _dataController,
-            decoration: InputDecoration(
-              labelText: 'Selecione a Data',
-              hintText: 'DD/MM/AAAA',
-            ),
-            readOnly: true, // Faz com que o campo seja somente leitura
-            onTap: () => _selecionarData(context), // Abre o DatePicker
-          ),
+          child: ElevatedButton(
+            onPressed: (){
+              showCupertinoModalPopup(
+                context: context,
+                builder: (context){
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text('Sair'),
+                        ),
+                        Expanded(
+                          child: CupertinoDatePicker(
+                            initialDateTime: _dataSelecionada,
+                            mode: CupertinoDatePickerMode.date,
+                            maximumDate: DateTime.now(),
+                            onDateTimeChanged: (date){
+                              setState(() {
+                                _dataSelecionada = date;
+                              });
+                            }
+                          )
+                        )
+                      ],
+                    ),
+                  );
+                }
+              );
+            },
+            child: Text(_dataSelecionada.toString())
+          )
         )
       ),
     );
